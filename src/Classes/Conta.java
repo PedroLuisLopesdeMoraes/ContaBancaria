ef7bc012678a780d2bbb4245dfb1fdbdeda6e3e8
pllmoraes <pedro2012sjc@gmail.com>
2024-08-19 15:56:35 -0300

    método de depositar

--- src/Classes/Conta.java
+++ src/Classes/Conta.java
@@ -6,5 +6,27 @@ public class Conta {
     private double saldoConta;
 
 
+    public String getCliente() {
+        return cliente;
+    }
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+    public int getNumConta() {
+        return numConta;
+    }
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+    public double getSaldoConta() {
+        return saldoConta;
+    }
+    public void setSaldoConta(double saldoConta) {
+        this.saldoConta = saldoConta;
+    }
+
+    public double deposita (double valor){
+        return saldoConta = valor + saldoConta;
+    }
 
 }
--- src/Teste/TesteContas.java
+++ src/Teste/TesteContas.java
@@ -1,7 +1,18 @@
 package Teste;
 
+import Classes.Conta;
+
 public class TesteContas {
     public static void main(String[] args) {
+        Conta c1 = new Conta();
+        c1.setCliente("Rafael");
+        c1.setNumConta(1234);
+        c1.setSaldoConta(1000);
 
+        System.out.println(c1.getSaldoConta());
+
+        c1.deposita(100);
+
+        System.out.println("novo saldo: " + c1.getSaldoConta());
     }
 }
